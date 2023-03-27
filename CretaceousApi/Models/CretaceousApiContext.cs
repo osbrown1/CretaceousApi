1eb83075aa05af1ddec8c170003d6075944bcfe3
osbrown1 <socknot@live.com>
2023-03-26 22:23:23 -0700

    update

---
+++ CretaceousApi/Models/CretaceousApiContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CretaceousApi.Models
+{
+  public class CretaceousApiContext : DbContext
+  {
+    public DbSet<Animal> Animals { get; set; }
+
+    public CretaceousApiContext(DbContextOptions<CretaceousApiContext> options) : base(options)
+    {
+    }
+  }
+}
